45404ecdcb862fcceb49bf51b06d5dae4d0bf354
SaxDev <berksedat123@gmail.com>
2022-09-14 14:10:30 +0300

    Fixes

--- src/commands/general/connect.js
+++ src/commands/general/connect.js
@@ -18,9 +18,9 @@ module.exports = {
 					} else if (client.temp[0][0] == interaction.guild.id){
 						interaction.reply("Server already in a queue.");
 						return;
-					} else {client.temp.push([interaction.guild.id,interaction.channelId]);
-						console.log("second client.temp")}
-						console.log("array added");
+					} else {
+						client.temp.push([interaction.guild.id,interaction.channelId]);
+					}
 				}
 
 				if(client.temp.length == 2) {
